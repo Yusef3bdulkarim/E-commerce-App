c6c9dc482b456aaa33c63721c4e9c1da7a30f932
Yusef3bdulkarim <hamadayosseef@gmail.com>
2026-02-14 21:25:57 +0200

    feat: setup firebase and android configuration

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,23 @@ import 'package:ecommerceapp/features/shop/logic/cubit/home_cubit.dart';
 import 'package:ecommerceapp/navigation_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // تأكد من أن Firebase يتم تشغيله بعد السطر السابق
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MultiBlocProvider(
       providers: [
-
         BlocProvider<NetworkManagerCubit>(
           create: (context) => NetworkManagerCubit(),
         ),
         BlocProvider<HomeCubit>(create: (context) => HomeCubit()),
         BlocProvider<NavBarCubit>(create: (context) => NavBarCubit()),
-
       ],
       child: const MyApp(),
     ),
